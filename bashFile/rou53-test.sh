7b279e481dab5f46a24faab5b690bfb025947f4c
Terence <terence.taih@gmail.com>
2014-08-20 21:52:49 +0700

    only run on 1 servlet

--- bashFile/rou53-test.sh
+++ bashFile/rou53-test.sh
@@ -7,20 +7,6 @@ echo "LATENCY NORMAL: " $i
 curl --no-sessionid -s -w '\nLookup time:\t%{time_namelookup}\nConnect time:\t%{time_connect}\nAppCon time:\t%{time_appconnect}\nRedirect time:\t%{time_redirect}\nPreXfer time:\t%{time_pretransfer}\nStartXfer time:\t%{time_starttransfer}\n\nTotal time:\t%{time_total}\n' -o /dev/null http://takipi-route53-testing-latency.com/takipi-aws/speed/normal
 done
 
-for i in {1..10}
-do
-sudo /etc/init.d/nscd restart
-echo "LATENCY WAIT 100ms: " $i
-curl --no-sessionid -s -w '\nLookup time:\t%{time_namelookup}\nConnect time:\t%{time_connect}\nAppCon time:\t%{time_appconnect}\nRedirect time:\t%{time_redirect}\nPreXfer time:\t%{time_pretransfer}\nStartXfer time:\t%{time_starttransfer}\n\nTotal time:\t%{time_total}\n' -o /dev/null http://takipi-route53-testing-latency.com/takipi-aws/speed/wait100
-done
-
-for i in {1..10}
-do
-sudo /etc/init.d/nscd restart
-echo "LATENCY DOWNLOAD 1Mb: " $i
-curl --no-sessionid -s -w '\nLookup time:\t%{time_namelookup}\nConnect time:\t%{time_connect}\nAppCon time:\t%{time_appconnect}\nRedirect time:\t%{time_redirect}\nPreXfer time:\t%{time_pretransfer}\nStartXfer time:\t%{time_starttransfer}\n\nTotal time:\t%{time_total}\n' -o /dev/null http://takipi-route53-testing-latency.com/takipi-aws/speed/res1mb
-done
-
 echo "End Running in LATENCY domain"
 echo "======================================================="
 echo "Running in GEO domain"
@@ -30,20 +16,6 @@ do
 sudo /etc/init.d/nscd restart
 echo "GEO NORMAL: " $i
 curl --no-sessionid -s -w '\nLookup time:\t%{time_namelookup}\nConnect time:\t%{time_connect}\nAppCon time:\t%{time_appconnect}\nRedirect time:\t%{time_redirect}\nPreXfer time:\t%{time_pretransfer}\nStartXfer time:\t%{time_starttransfer}\n\nTotal time:\t%{time_total}\n' -o /dev/null http://takipi-route53-testing-geo.com/takipi-aws/speed/normal
-done
-
-for i in {1..10}
-do
-sudo /etc/init.d/nscd restart
-echo "GEO WAIT 100ms: " $i
-curl --no-sessionid -s -w '\nLookup time:\t%{time_namelookup}\nConnect time:\t%{time_connect}\nAppCon time:\t%{time_appconnect}\nRedirect time:\t%{time_redirect}\nPreXfer time:\t%{time_pretransfer}\nStartXfer time:\t%{time_starttransfer}\n\nTotal time:\t%{time_total}\n' -o /dev/null http://takipi-route53-testing-geo.com/takipi-aws/speed/wait100
-done
-
-for i in {1..10}
-do
-sudo /etc/init.d/nscd restart
-echo "GEO DOWNLOAD 1Mb: " $i
-curl --no-sessionid -s -w '\nLookup time:\t%{time_namelookup}\nConnect time:\t%{time_connect}\nAppCon time:\t%{time_appconnect}\nRedirect time:\t%{time_redirect}\nPreXfer time:\t%{time_pretransfer}\nStartXfer time:\t%{time_starttransfer}\n\nTotal time:\t%{time_total}\n' -o /dev/null http://takipi-route53-testing-geo.com/takipi-aws/speed/res1mb
 done
 
 echo "End Running in GEO domain"
